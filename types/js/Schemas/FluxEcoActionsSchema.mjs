bf34220e6e50d3ee5595a4a55f09d97c04f14542
Martin Studer <martin.studer@medi.ch>
2023-04-04 12:32:57 +0200

    begin refactoring

---
+++ types/js/Schemas/FluxEcoActionsSchema.mjs
@@ -0,0 +1,18 @@
+/**
+ * @typedef {Object} FluxEcoActionsSchema
+ * @property {Object.<string, FluxEcoActionSchema>}
+ */
+
+/**
+ * @typedef {Object} FluxEcoActionSchema - A schema for defining an action in FluxEco.
+ * @property {string} path - The path of the action.
+ * @property {Object.<string, FluxEcoActionParameterSchema>} parameters - arameters - The parameters of the action, with each key being a string and the value being a FluxEcoActionParameterSchema.
+ * @property {Object} response - The response of the action.
+ * @property {string} response.contentType - The content type of the response.
+ */
+
+/**
+ * @typedef {Object} FluxEcoActionParameterSchema - A schema for defining a parameter in a FluxEco action.
+ * @property {string} in - The location of the parameter ("path" or "query").
+ * @property {string} type - The data type of the parameter ("string" or "number").
+ */
---
+++ types/js/Schemas/FluxEcoFilePathsSchema.mjs
@@ -0,0 +1,11 @@
+/**
+ * FluxEcoFilePathsSchema
+ *
+ * @typedef {Object} FluxEcoFilePathsSchema
+ * @property {Object.<string, FluxEcoFilePathSchema>} - Configuration for public files, where the key is the directory- or file-path
+ */
+
+/**
+ * @typedef {Object} FluxEcoFilePathSchema
+ * @property {string} contentType - The content type of the file(s).
+ */
